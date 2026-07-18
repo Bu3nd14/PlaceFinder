30a8ffbd20af083ca6accd9a0d5516e92af16076
Rob <roberto.carmeli@gmail.com>
2026-07-18 09:06:51 +0200

    Initial Commit

---
+++ PlaceFinder/PlaceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceFinderApp.swift
+//  PlaceFinder
+//
+//  Created by Roberto on 18.07.26.
+//
+
+import SwiftUI
+
+@main
+struct PlaceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
